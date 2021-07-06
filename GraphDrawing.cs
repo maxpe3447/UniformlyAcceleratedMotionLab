015adede143a7be6e679872a5cf5226875c3111d
Max <mpechersky16@gmail.com>
2021-07-06 13:39:33 +0300

    + class GraphDrawing for drawing triangles

---
+++ GraphDrawing.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace UniformlyAcceleratedMotionLab
+{
+    class GraphDrawing
+    {
+        private Graphics graph;
+        private Bitmap bmp;
+
+        private int _h, _l;
+        private double _s;
+
+        private System.Windows.Forms.PictureBox pictureBox;
+
+        private Point startPoint, secondPoint, arbitrarilyPoint, newArbitrarilyPoint;
+        public double Angle
+        {
+            get { return CalcAnAngle(); }
+        }
+
+        /// <summary>
+        /// $"H = {H} m" or use GetInfoOfSize()
+        /// </summary>
+        public int H
+        {
+            get { return _h; }
+            private set { _h = value; }
+        }
+
+        /// <summary>
+        /// $"L = {L} m" or use GetInfoOfSize()
+        /// </summary>
+        public int L
+        {
+            get { return _l; }
+            private set { _l = value; }
+        }
+
+        /// <summary>
+        /// $"S = {S} m" or use GetInfoOfSize()
+        /// </summary>
+        public double S
+        {
+            get { return _s; }
+            private set { _s = value; }
+        }
+        public GraphDrawing(System.Windows.Forms.PictureBox pictureBox)
+        {
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graph = Graphics.FromImage(bmp);
+
+            this.pictureBox = pictureBox;
+        }
+        public void DrawTriangle()
+        {
+            graph.DrawLine(new Pen(Color.Black, 3), startPoint, secondPoint);
+            graph.DrawLine(new Pen(Color.Black, 3), arbitrarilyPoint, secondPoint);
+            graph.DrawLine(new Pen(Color.Black, 3), arbitrarilyPoint, startPoint);
+
+            int radius = 25;
+            RectangleF phosiksBody = new RectangleF(arbitrarilyPoint.X - radius, arbitrarilyPoint.Y - radius, radius, radius);
+            graph.DrawEllipse(new Pen(Color.Blue, 3), phosiksBody);
+            pictureBox.Image = bmp;
+        }
+
+        public void CreateNewTriangle(System.Windows.Forms.Form form)
+        {
+            newArbitrarilyPoint = System.Windows.Forms.Cursor.Position;
+            newArbitrarilyPoint.X -= form.Location.X;
+            newArbitrarilyPoint.Y -= form.Location.Y;
+
+            if (newArbitrarilyPoint != arbitrarilyPoint)
+            {
+                graph.Clear(Color.White);
+
+                newArbitrarilyPoint.X -= 10;
+                newArbitrarilyPoint.Y -= 62;
+                arbitrarilyPoint = newArbitrarilyPoint;
+
+                secondPoint.X = newArbitrarilyPoint.X;
+
+                H = secondPoint.Y - arbitrarilyPoint.Y;
+                L = secondPoint.X - startPoint.X;
+                S = Math.Sqrt(Math.Pow(L, 2) + Math.Pow(H, 2));
+
+                DrawTriangle();
+            }
+        }
+
+        public void FirstCreate()
+        { 
+            graph.Clear(Color.White);
+
+            Random rand = new Random();
+
+            startPoint.X = rand.Next(pictureBox.Location.X + 30, pictureBox.Location.X + 30 + 50);
+            startPoint.Y = rand.Next(pictureBox.Location.Y + pictureBox.Height - 60, pictureBox.Location.Y + pictureBox.Height - 40);
+
+            int widthOfMainTriangle = rand.Next(pictureBox.Width / 2, (int)pictureBox.Width * 2 / 3);
+            secondPoint.X = startPoint.X + widthOfMainTriangle;
+            secondPoint.Y = startPoint.Y;
+
+            int heightOfMainTriangle = rand.Next(pictureBox.Location.Y + 50, pictureBox.Location.Y + 70);
+            arbitrarilyPoint.X = secondPoint.X;
+            arbitrarilyPoint.Y = heightOfMainTriangle;
+
+            H = secondPoint.Y - heightOfMainTriangle;
+            L = widthOfMainTriangle;
+            S = Math.Sqrt(Math.Pow(L, 2) + Math.Pow(H, 2));
+        }
+        private double CalcAnAngle() => /*(int)*/Math.Floor((Math.Atan(H / L)) * 180.0 / Math.PI) % 360;
+        public string GetInfoOfSize() => $"L = {L} m\nH = {H} m\nS = {S.ToString("0.000")} m\n-------------\n";
+        public string GetCursorPos() => $"Pos: {newArbitrarilyPoint.X} -> {newArbitrarilyPoint.Y}\n";
+
+    }
+}
--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -36,8 +36,7 @@ namespace UniformlyAcceleratedMotionLab
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.debugLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.toolStrip1.SuspendLayout();
@@ -62,7 +61,7 @@ namespace UniformlyAcceleratedMotionLab
             this.toolStripButton2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1079, 33);
+            this.toolStrip1.Size = new System.Drawing.Size(1619, 57);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -72,7 +71,7 @@ namespace UniformlyAcceleratedMotionLab
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 52);
             this.toolStripButton1.Text = "toolStripButton1";
             // 
             // toolStripButton2
@@ -95,6 +94,7 @@ namespace UniformlyAcceleratedMotionLab
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(254, 506);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(245, 63);
@@ -103,25 +103,15 @@ namespace UniformlyAcceleratedMotionLab
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(779, 385);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 29);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(784, 431);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 29);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.debugLabel.AutoSize = true;
+            this.debugLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.debugLabel.Location = new System.Drawing.Point(742, 365);
+            this.debugLabel.Name = "label2";
+            this.debugLabel.Size = new System.Drawing.Size(93, 44);
+            this.debugLabel.TabIndex = 5;
+            this.debugLabel.Text = "NaN";
             // 
             // label3
             // 
@@ -139,8 +129,7 @@ namespace UniformlyAcceleratedMotionLab
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1079, 635);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.debugLabel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.toolStrip1);
@@ -164,8 +153,7 @@ namespace UniformlyAcceleratedMotionLab
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label debugLabel;
         private System.Windows.Forms.Label label3;
     }
 }
