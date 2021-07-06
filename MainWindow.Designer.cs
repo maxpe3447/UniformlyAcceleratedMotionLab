f099f096552abc525d8792dd9c650cb72c603fbd
Max <mpechersky16@gmail.com>
2021-07-06 12:25:47 +0300

    + working triang.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace UniformlyAcceleratedMotionLab
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(697, 416);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1079, 33);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(34, 33);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(747, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(298, 265);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 63);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(779, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(784, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(154, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(623, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Лабораторна робота на ему: \"Рівноприскорений рух\"";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 635);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Рівноприскорений рух";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniformlyAcceleratedMotionLab
+{
+    public partial class MainWindow : Form
+    {
+        Graphics graph;
+        Bitmap bmp; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            //graph = this.CreateGraphics();
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graph = Graphics.FromImage(bmp);
+            //DrawLines();
+
+        }
+        private double GetAnAngle(double h, double l) => (int)Math.Floor((Math.Atan(h/l)) * 180.0 / Math.PI) % 360; 
+        //{
+
+        //    return 
+        //}
+        private void DrawTriangle()
+        {
+            graph.DrawLine(new Pen(Color.Black, 3), startPoint, secondPoint);
+            graph.DrawLine(new Pen(Color.Black, 3), arbitrarilyPoint, secondPoint);
+            graph.DrawLine(new Pen(Color.Black, 3), arbitrarilyPoint, startPoint);
+
+            int radius = 25;
+            RectangleF phosiksBody = new RectangleF(arbitrarilyPoint.X - radius, arbitrarilyPoint.Y - radius, radius, radius);
+            graph.DrawEllipse(new Pen(Color.Blue, 3), phosiksBody);
+            pictureBox1.Image = bmp;
+        }
+
+        private Point startPoint, secondPoint, arbitrarilyPoint;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Point newArbitrarilyPoint = Cursor.Position;
+            newArbitrarilyPoint.X -= this.Location.X;
+            newArbitrarilyPoint.Y -= this.Location.Y;
+
+            if (newArbitrarilyPoint != arbitrarilyPoint)
+            {
+                graph.Clear(Color.White);
+                //
+                label1.Text = $"Pos: {newArbitrarilyPoint.X} -> {newArbitrarilyPoint.Y}";
+                
+                //
+                newArbitrarilyPoint.X -= 10;
+                newArbitrarilyPoint.Y -= 62;
+                arbitrarilyPoint = newArbitrarilyPoint;
+                
+                secondPoint.X = newArbitrarilyPoint.X;
+
+                int _H =  secondPoint.Y - arbitrarilyPoint.Y;
+                int _L = secondPoint.X - startPoint.X;
+                double _S = Math.Sqrt(Math.Pow(_L, 2) + Math.Pow(_H, 2));
+               
+                richTextBox1.AppendText($"L = {_L} m\nH = {_H} m\nS = {_S.ToString("0.000")} m\n-------------\n");
+                richTextBox1.AppendText(arbitrarilyPoint.ToString());
+                DrawTriangle();
+                label2.Text = GetAnAngle(_H, _L).ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            graph.Clear(Color.White);
+
+            Random rand = new Random();
+
+            startPoint.X = rand.Next(pictureBox1.Location.X + 30, pictureBox1.Location.X + 30 + 50);
+            startPoint.Y = rand.Next(pictureBox1.Location.Y + pictureBox1.Height - 60, pictureBox1.Location.Y + pictureBox1.Height -40);
+            
+            int widthOfMainTriangle = rand.Next(pictureBox1.Width / 2, (int) pictureBox1.Width * 2 / 3);
+            secondPoint.X = startPoint.X + widthOfMainTriangle;//widthOfMainTriangle;
+            secondPoint.Y = startPoint.Y;
+
+            int heightOfMainTriangle = rand.Next(pictureBox1.Location.Y+50, pictureBox1.Location.Y+70);
+            arbitrarilyPoint.X = secondPoint.X;
+            arbitrarilyPoint.Y = heightOfMainTriangle;
+
+            int _H = secondPoint.Y - heightOfMainTriangle;
+            int _L = widthOfMainTriangle;
+            double _S = Math.Sqrt(Math.Pow(_L, 2) + Math.Pow(_H, 2));
+           
+            richTextBox1.AppendText($"L = {_L} m\nH = {_H} m\nS = {_S.ToString("0.000")} m\n-------------\n");
+            richTextBox1.AppendText(arbitrarilyPoint.ToString());
+            DrawTriangle();
+
+
+            //Bitmap bitmap = new Bitmap(pictureBox1.Width + 1, pictureBox1.Height + 1, graph);
+            //pictureBox1.Image = bitmap;
+        }
+    }
+}
